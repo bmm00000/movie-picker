e201407055e616423bd17683566381a11a10dbaf
bmm00000 <bmm00000@icloud.com>
2020-12-14 22:03:09 +0000

    add main input and debouncer

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '79a0b990',
+			s: searchTerm,
+		},
+	});
+	console.log(response.data.Search);
+};
+
+const input = document.querySelector('input');
+
+const debounce = (func, delay = 1000) => {
+	let timeoutId;
+	return (...args) => {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+			func.apply(null, args);
+		}, delay);
+	};
+};
+
+const onInput = (event) => {
+	fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 500));
