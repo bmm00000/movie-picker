68ab497e9d1218a1e240574a8739e476f0d5c3be
bmm00000 <bmm00000@icloud.com>
2020-12-15 21:59:20 +0000

    add reusable autocomplete widget

--- index.js
+++ index.js
@@ -5,25 +5,77 @@ const fetchData = async (searchTerm) => {
 			s: searchTerm,
 		},
 	});
-	console.log(response.data.Search);
+
+	if (response.data.Error) {
+		return [];
+	}
+
+	return response.data.Search;
 };
 
-const input = document.querySelector('input');
+createAutocomplete({
+	root: document.querySelector('.autocomplete'),
+	renderOption(movie) {
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+		return `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+    `;
+	},
+	onOptionSelect(movie) {
+		onMovieSelect(movie);
+	},
+	inputValue(movie) {
+		return movie.Title;
+	},
+});
 
-const debounce = (func, delay = 1000) => {
-	let timeoutId;
-	return (...args) => {
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-		}
-		timeoutId = setTimeout(() => {
-			func.apply(null, args);
-		}, delay);
-	};
+const onMovieSelect = async (movie) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '79a0b990',
+			i: movie.imdbID,
+		},
+	});
+
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
 };
 
-const onInput = (event) => {
-	fetchData(event.target.value);
+const movieTemplate = (movieDetail) => {
+	return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}" alt="Movie poster" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-info">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-info">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-info">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-info">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+        <article class="notification is-info">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+    `;
 };
-
-input.addEventListener('input', debounce(onInput, 500));
