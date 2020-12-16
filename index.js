7917ffd94f7d0b1c583728617b29c150595f6fc7
bmm00000 <bmm00000@icloud.com>
2020-12-16 15:47:08 +0000

    add button to search again and final styles

--- index.js
+++ index.js
@@ -63,22 +63,51 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 
 	if (leftMovie && rightMovie) {
 		runComparison();
+		document.querySelector('#refresh').classList.remove('is-hidden');
 	}
 };
 
 const runComparison = () => {
-	console.log('tiome to compare');
+	const leftSideStats = document.querySelectorAll(
+		'#left-summary .notification'
+	);
+	const rightSideStats = document.querySelectorAll(
+		'#right-summary .notification'
+	);
+
+	leftSideStats.forEach((leftStat, index) => {
+		const rightStat = rightSideStats[index];
+
+		const leftSideValue = parseInt(leftStat.dataset.value);
+		const rightSideValue = parseInt(rightStat.dataset.value);
+
+		if (rightSideValue > leftSideValue) {
+			leftStat.classList.remove('is-info');
+			leftStat.classList.add('is-danger');
+			rightStat.classList.remove('is-info');
+			rightStat.classList.add('is-success');
+		} else {
+			rightStat.classList.remove('is-info');
+			rightStat.classList.add('is-danger');
+			leftStat.classList.remove('is-info');
+			leftStat.classList.add('is-success');
+		}
+	});
 };
 
 const movieTemplate = (movieDetail) => {
-	const dollars = parseInt(
-		movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '')
-	);
 	const metascore = parseInt(movieDetail.Metascore);
 	const imdbRating = parseFloat(movieDetail.imdbRating);
 	const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+	const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+		const value = parseInt(word);
+		if (isNaN(value)) {
+			return prev;
+		} else {
+			return prev + value;
+		}
+	}, 0);
 
-	console.log(metascore, imdbRating, imdbVotes);
 	return `
         <article class="media">
             <figure class="media-left">
@@ -94,23 +123,19 @@ const movieTemplate = (movieDetail) => {
                 </div>
             </div>
         </article>
-        <article class="notification is-info">
+        <article data-value=${awards} class="notification is-info">
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-info">
-            <p class="title">${movieDetail.BoxOffice}</p>
-            <p class="subtitle">Box Office</p>
-        </article>
-        <article class="notification is-info">
+        <article data-value=${metascore} class="notification is-info">
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-info">
+        <article data-value=${imdbRating} class="notification is-info">
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article class="notification is-info">
+        <article data-value=${imdbVotes} class="notification is-info">
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
