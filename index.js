d034a5774ca447f9925821353a01d52830e0b5c6
bmm00000 <bmm00000@icloud.com>
2021-01-03 15:12:17 +0100

    add https to api url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const autoCompleteConfig = {
 		return movie.Title;
 	},
 	async fetchData(searchTerm) {
-		const response = await axios.get('http://www.omdbapi.com/', {
+		const response = await axios.get('https://www.omdbapi.com/', {
 			params: {
 				apikey: '79a0b990',
 				s: searchTerm,
@@ -46,7 +46,7 @@ createAutocomplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-	const response = await axios.get('http://www.omdbapi.com/', {
+	const response = await axios.get('https://www.omdbapi.com/', {
 		params: {
 			apikey: '79a0b990',
 			i: movie.imdbID,
